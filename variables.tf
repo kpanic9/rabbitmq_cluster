506e061bb3927ab40f71c35f3a59622b4655e29f
Namesh Sanjitha <tns.xiii@gmail.com>
2018-10-11 18:01:33 +0530

    added variable descriptions

--- variables.tf
+++ variables.tf
@@ -5,26 +5,31 @@
  */
 
 
+# ami of the cluster nodes
 variable "node_ami" {
 	default = ""
 }
 
 
+# aws instance type for nodes
 variable "instance_type" {
 	default = ""
 }
 
 
+# key pair name for logging into the cluster nodes
 variable "key" {
 	default = ""
 }
 
 
+# private subnet id to provision the cluster
 variable "sunbet" {
 	default = ""
 }
 
 
+# rabbitmq node security group
 variable "security_group" {
 	default = ""
 }
