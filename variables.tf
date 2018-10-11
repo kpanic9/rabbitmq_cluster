78c3aa865beccad5e1b9c525ff168773c53b246d
Namesh Sanjitha <tns.xiii@gmail.com>
2018-10-11 17:58:36 +0530

    defining parameters of the cluster

---
+++ variables.tf
@@ -0,0 +1,31 @@
+/*
+ * Configuration Variables
+ *
+ *
+ */
+
+
+variable "node_ami" {
+	default = "ami-0782017a917e973e7"
+}
+
+
+variable "instance_type" {
+	default = "t2.micro"
+}
+
+
+variable "key" {
+	default = ""
+}
+
+
+variable "sunbet" {
+	default = ""
+}
+
+
+variable "security_group" {
+	default = ""
+}
+
