817fb73e3d42ced724cd86f416558fa428296fcd
Namesh Sanjitha <tns.xiii@gmail.com>
2018-10-14 19:55:53 +0530

    added comments

--- outputs.tf
+++ outputs.tf
@@ -5,13 +5,13 @@
  */
 
 
-#  
+# priting master node ip  
 output "rabbitmq_master_ip" {
 	value = "${aws_instance.rabbitmq_master.private_dns}"
 }
 
 
-#
+# printing slave node ip
 output "rabbitmq_slave_ip" {
 	value = "${aws_instance.rabbitmq_slave.private_dns}"
 }
