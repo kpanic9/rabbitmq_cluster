1038ab8a0540731649dae9088470682ff899a604
Namesh Sanjitha <tns.xiii@gmail.com>
2018-10-14 19:55:22 +0530

    added notes

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,17 @@
 /*
- * Outputs
- *
+ * Outputs:
+ * 	1. ip address of master node
+ * 	2. ip address of slave node
  */
 
- 
+
+#  
 output "rabbitmq_master_ip" {
 	value = "${aws_instance.rabbitmq_master.private_dns}"
 }
 
 
+#
 output "rabbitmq_slave_ip" {
 	value = "${aws_instance.rabbitmq_slave.private_dns}"
 }
